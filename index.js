1f206046696f99db9d8c22a69ad5684d66076cbe
loanG <loan.godard031@gmail.com>
2021-04-12 23:29:28 +0200

    allowing CORS

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 /**
  * Socket io
  */
